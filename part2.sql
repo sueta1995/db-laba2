764c3a116534aafa401c7c1d86d8d3e8256b5d36
sueta1995 <39643866+sueta1995@users.noreply.github.com>
2022-10-11 23:50:03 +0300

    Update part2.sql

--- part2.sql
+++ part2.sql
@@ -1,5 +1,5 @@
 CREATE TABLE projects (
-    id integer PRIMARY KEY,
+    id serial PRIMARY KEY,
     cipher integer UNIQUE NOT NULL,
     name text UNIQUE NOT NULL,
     deadline timestamp NOT NULL,
@@ -7,13 +7,13 @@ CREATE TABLE projects (
 );
 
 CREATE TABLE employees (
-    id integer PRIMARY KEY,
+    id serial PRIMARY KEY,
     full_name text UNIQUE NOT NULL,
     function text NOT NULL
 );
 
 CREATE TABLE assignments (
-    id integer PRIMARY KEY,
+    id serial PRIMARY KEY,
     laboriousness integer NOT NULL,
     issue_date timestamp NOT NULL,
     planned_expiration_date timestamp NOT NULL,
@@ -21,4 +21,3 @@ CREATE TABLE assignments (
     project_id integer REFERENCES projects (id) NOT NULL,
     employee_id integer REFERENCES employees (id) NOT NULL
 );
-    
