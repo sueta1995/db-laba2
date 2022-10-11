5ef09192d862b1efb7bde7e203d1d2ae3b313b6b
sueta1995 <39643866+sueta1995@users.noreply.github.com>
2022-10-11 22:32:24 +0300

    Update part2.sql

--- part2.sql
+++ part2.sql
@@ -1,24 +1,24 @@
 CREATE TABLE projects (
     id integer PRIMARY KEY,
-    cipher integer UNIQUE,
-    name text UNIQUE,
-    deadline timespamp,
-    laboriousness integer
+    cipher integer UNIQUE NOT NULL,
+    name text UNIQUE NOT NULL,
+    deadline timespamp NOT NULL,
+    laboriousness integer NOT NULL
 );
 
 CREATE TABLE employees (
     id integer PRIMARY KEY,
-    full_name text UNIQUE,
-    function text
+    full_name text UNIQUE NOT NULL,
+    function text NOT NULL
 );
 
 CREATE TABLE assignments (
     id integer PRIMARY KEY,
-    laboriousness integer,
-    issue_date timestamp,
-    planned_expiration_date timestamp,
-    real_expiration_date timestamp,
-    project_id integer REFERENCES projects (id),
-    employee_id integer REFERENCES employees (id)
+    laboriousness integer NOT NULL,
+    issue_date timestamp NOT NULL,
+    planned_expiration_date timestamp NOT NULL,
+    real_expiration_date timestamp NOT NULL,
+    project_id integer REFERENCES projects (id) NOT NULL,
+    employee_id integer REFERENCES employees (id) NOT NULL
 );
     
