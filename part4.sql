7c07f01cf35beea25c7b09c5aa0900df07285cad
sueta1995 <39643866+sueta1995@users.noreply.github.com>
2022-10-26 01:19:26 +0300

    Update part4.sql

--- part4.sql
+++ part4.sql
@@ -1,24 +1,21 @@
-CREATE FUNCTION emp_stamp() RETURNS trigger AS $emp_stamp$
+CREATE FUNCTION employees_check() RETURNS trigger AS $employees_check$
     BEGIN
-        -- Проверить, что указаны имя сотрудника и зарплата
-        IF NEW.empname IS NULL THEN
-            RAISE EXCEPTION 'empname cannot be null';
+        -- Проверить, что указаны имя сотрудника и его должность
+        IF NEW.full_name IS NULL THEN
+            RAISE EXCEPTION 'Имя сотрудника не может быть NULL';
         END IF;
-        IF NEW.salary IS NULL THEN
-            RAISE EXCEPTION '% cannot have null salary', NEW.empname;
+        IF NEW.function IS NULL THEN
+            RAISE EXCEPTION '% не может иметь должность со значением NULL', NEW.empname;
         END IF;
 
-        -- Кто будет работать, если за это надо будет платить?
-        IF NEW.salary < 0 THEN
+        -- Проверка уникальности имени нового сотрудника
+        IF NEW.full_name IN (SELECT full_name FROM employees) THEN
             RAISE EXCEPTION '% cannot have a negative salary', NEW.empname;
         END IF;
-
-        -- Запомнить, кто и когда изменил запись
-        NEW.last_date := current_timestamp;
-        NEW.last_user := current_user;
+        
         RETURN NEW;
     END;
-$emp_stamp$ LANGUAGE plpgsql;
+$employees_check$ LANGUAGE plpgsql;
 
-CREATE TRIGGER emp_stamp BEFORE INSERT OR UPDATE ON emp
-    FOR EACH ROW EXECUTE FUNCTION emp_stamp();
+CREATE TRIGGER employees_check BEFORE INSERT OR UPDATE ON employees
+    FOR EACH ROW EXECUTE FUNCTION employees_check();
